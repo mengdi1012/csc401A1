98a7af211af16c08f6e99c76dd20ed3eace29031
Mengdi Shi <mengdishi@shimengdideMacBook-Pro.local>
2017-02-04 23:59:25 -0500

    twtt

--- twtt.py
+++ twtt.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
-
+import os
 import sys
 import re
+import NLPlib
 
 def twtt1(tw):
    
@@ -22,20 +23,89 @@ def twtt4(tw):
    return tw
 
 
-def twtt5(tw):
-   return tw
-
+def twtt5(tw,abbr):
    
+   listwords= tw.split()
+   sentences = ''
+   for i in range(len(listwords)):
+       word = listwords[i]
+       sentences += word+' '
+       lastchar = word[-1]
+       if lastchar == '?' or lastchar == '!':
+           sentences += '\n'
+        
+       elif lastchar == '.':
+           if not abbr.has_key(word) and i+1 < len(listwords) \
+                   and listwords[i+1][0].isupper(): 
+              sentences += '\n' 
+   return sentences
+
+def twtt7(tw,abbr):
+    sent_list = tw.split('\n')
+    sentences = ''
+    for sentence in sent_list:
+        words = sentence.split()
+        for word in words:
+            if abbr.has_key(word):
+               sentences += word+' '
+            else:   
+               m = re.match(r'([^\w]*)(\w+)([^\w].*)', word)
+               if not m:
+                   sentences +=word + ' '
+               else:
+                   if not m.group(1):
+                       sentences += m.group(2)+' '+m.group(3)+' '
+                   else:
+                       sentences += m.group(1)+' '+m.group(2)+' '+m.group(3)+' '
+        
+        if sentence:
+            sentences += '\n'
+    return sentences
+ 
+def twtt8(tw,tagger):
+    sent_list = tw.split('\n')
+    result = ''
+    
+    for sentence in sent_list:
+        word_list = sentence.split()
+        tags = tagger.tag(word_list)
+        for i in range(len(word_list)):
+            result += word_list[i]+'/'+tags[i] +' '
+        result +=  '\n'
+     
+    return result.strip()
+
 if __name__ == "__main__":
 
    if len(sys.argv) != 4:
-      print "Usage: %s <training-data-filr> <student#> <output-file>" %(sys.argv[0])
-      #sys.exit(0)
-
-   rawfile = open("/u/cs401/A1/tweets/training.1600000.processed.noemoticon.csv","r")
-   resultfile = open("train.twt","w")
-   X = int("1001601019")%80
+       print "Usage: %s <training-data-filr> <student#> <output-file>" %(sys.argv[0])
+       sys.exit(0)
+      
+   if not os.path.exists(sys.argv[1]):
+       print sys.argv[1] + " not exists"
+       sys.exit(0)
+       
+   fn_abbr = "/u/cs401/Wordlists/abbrev.english"
+   if not os.path.exists(fn_abbr):
+       print fn_abbr + " not found"
+       exit(0)
+   fileabbr = open(fn_abbr,"r")
+   abbr = {}
+   while True:
+       line = fileabbr.readline()
+       if not line:
+           break
+       line = line.strip()
+       abbr[line] = True    
+   fileabbr.close()
+   
+   rawfile = open(sys.argv[1],"r")
+   resultfile = open(sys.argv[3],"w")
+   X = int(sys.argv[2])%80
    count = 0
+
+   tagger = NLPlib.NLPlib()
+   
    while count <= 800000+X*10000+9999:
       line = rawfile.readline()
       if (count >= X*10000 and count <= X*10000+9999) \
@@ -47,8 +117,9 @@ if __name__ == "__main__":
          #print count-10000*X
          m = re.match(r'"(\d)"(,[^,]+){4},"(.+)"',twtt)
          print >>resultfile, '<A='+m.group(1)+'>'
-         twtt = twtt5(m.group(3))
-         
+         twtt = twtt5(m.group(3),abbr)
+         twtt = twtt7(twtt, abbr)
+         twtt = twtt8(twtt,tagger)
          
          print >>resultfile,twtt
 
